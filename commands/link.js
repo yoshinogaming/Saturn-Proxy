770e731007c254ce6beff0a4155c79949576e704
Eternity <66208385+EternityGT@users.noreply.github.com>
2021-06-20 12:40:19 +0700

    Update link.js

--- commands/link.js
+++ commands/link.js
@@ -4,7 +4,7 @@ const disbut = require('discord-buttons');
 
 exports.run = async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
-    .setTitle('Invite me!')
+    .setTitle('LINK!')
     .setThumbnail(client.user.displayAvatarURL({ size: 2048 }))
     .setColor(config.color)
     .setDescription(`Github ➔ [Click Here](https://github.com/)\nTop.gg ➔ [Click Here](https://top.gg/)\nDiscord ➔ [Click Here](https://discord.com/)\nYoutube ➔ [Click Here](https://youtube.com/)\nKite ➔ [Click Here](https://kite.com/)`)
