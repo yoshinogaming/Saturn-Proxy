4e6b7a10c3b3b43d8f9e97aa5da527f2f4133689
Zren <66208385+Zrenity@users.noreply.github.com>
2021-09-18 19:25:16 +0700

    dad

--- events/message.js
+++ events/message.js
@@ -1,4 +1,5 @@
 const config = require("../config.json");
+const ch = require("../channels.json");
 
 module.exports = (client, message) => {
   if (message.author.bot) return;
@@ -6,6 +7,15 @@ module.exports = (client, message) => {
   if (message.content.startsWith(`<@${client.user.id}>`) || message.content.startsWith(`<@!${client.user.id}>`)) {
     return message.channel.send(`Hello ${message.author}, you can start by typing **${config.prefix}help**!`);
   }
+
+  if (ch.ch1.includes(message.channel.id)) {
+    fetch(`https://api.monkedev.com/fun/chat?msg=${message.content}%20there!&uid=${message.author.id}`)
+      .then(response => response.json())
+      .then(data => {
+        message.reply(data.response)
+      })
+  }
+
   if (message.content.indexOf(client.config.prefix) !== 0) return;
 
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
