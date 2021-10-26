a69e0ae5de0959a98a1dd6b0aa3780024f4f5532
ZhyTopia <83191527+ZhyTopia@users.noreply.github.com>
2021-10-26 00:58:36 +0000

    lmaoaoao

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 @echo off
-title Giveaway Proxy 1.5
+title Saturn Proxy 1.5
 :top
 cls
 color 9
 echo ====================
-echo Giveaway Discord Bot 2021
+echo Discord Bot 2021
 echo Script By Zrenity1#0001
 echo Version 1.5
 echo ====================
